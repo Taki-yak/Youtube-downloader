a14b3b9109c3ec54cff30a631616d099d98b9251
taki <takiyakhlef49@gmail.com>
2024-11-01 21:16:28 +0100

    modify occure from taki's branch

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,8 +1,8 @@
 from pytube import YouTube 
 
-videolink=str(input("Link of The Youtube video :"))
+videolink=str(input("Youtube Video Link:"))
 yt = YouTube(videolink)
 print("Title:",yt.title)
 print("views:",yt.views)
 yd=yt.streams.get_highest_resolution()
-yd.download(r"your path to save the video")
+yd.download(r"your-path-to-save-the-video")
